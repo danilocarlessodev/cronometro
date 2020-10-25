1571aeea55c14be2bfd54ed75957de5d0f2a5484
Danilo Carlesso <danilocarlessodev@gmail.com>
2020-10-25 04:48:39 -0300

    Criada as funçôes de iniciar, pausar, parar e de construir o cronômetro!

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// Seleciona todos os elementos do cronômetro com base no ID passado no documento html!
+
+const visor = document.querySelector(".visor span");
+const iniciar = document.getElementById("iniciar");
+const pausar = document.getElementById("pausar");
+const parar = document.getElementById("parar");
+
+// Cria as variaveis dos dados com LET por causa do escopo, preciso de acesso a elas em todas funções, com const não daria certo
+
+let horas = 0;
+let minutos = 0;
+let segundos = 0;
+let cronometro;
+
+// Funcão que inicia o cronômetro
+
+function iniciarCronometro() {
+  cronometro = setInterval(() => {
+    segundos++;
+    constroeCronometro();
+  }, 1000);
+}
+
+// Função responsável por pausar o cronômetro
+
+function pausarCronometro() {
+  clearInterval(cronometro);
+}
+
+// Função de parar/resetar o cronômetro
+
+function pararCronometro() {
+  horas = 0;
+  minutos = 0;
+  segundos = 0;
+  visor.innerText = "00:00:00";
+  pausarCronometro();
+}
+
+// Função responsável por ficar atualizando o temporizador do cronômetro
+
+function constroeCronometro() {
+  if (segundos === 60) {
+    segundos = 0;
+    minutos++;
+  }
+
+  if (minutos === 60) {
+    minutos = 0;
+    horas++;
+  }
+
+  visor.innerText = `${horas < 10 ? "0" + horas : horas}:${
+    minutos < 10 ? "0" + minutos : minutos
+  }:${segundos < 10 ? "0" + segundos : segundos}`;
+}
